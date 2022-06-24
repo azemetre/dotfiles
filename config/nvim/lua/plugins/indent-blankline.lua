aa1f4070bc5b34db4c75dc5484fed6dedb56c090
Aaron Zemetres <azemetre@gmail.com>
2022-06-24 16:08:57 -0400

    nvim: indention for blank lines

---
+++ config/nvim/lua/plugins/indent-blankline.lua
@@ -0,0 +1,22 @@
+require("indent_blankline").setup({
+	viewport_buffer = 100,
+	filetype_exclude = {
+		"man",
+		"diagnosticpopup",
+		"lspinfo",
+		"checkhealth",
+		"TelescopePrompt",
+		"TelescopeResults",
+		"",
+	},
+	buftype_exclude = { "terminal" },
+	space_char_blankline = " ",
+	show_foldtext = false,
+	strict_tabs = true,
+	debug = true,
+	disable_with_nolist = true,
+	max_indent_increase = 1,
+	show_current_context = true,
+	show_current_context_start = true,
+	use_treesitter_scope = true,
+})
