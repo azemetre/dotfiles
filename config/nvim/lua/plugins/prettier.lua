27d1141e7178b5c6854a273ffc48491e0a9cb51a
azemetre <azemetre@gmail.com>
2022-09-11 19:30:40 -0400

    nvim: lua formatting, ignoring markdown

--- config/nvim/lua/plugins/prettier.lua
+++ config/nvim/lua/plugins/prettier.lua
@@ -1,19 +1,19 @@
 local prettier = require("prettier")
 
 prettier.setup({
-  bin = 'prettier', -- or `'prettierd'` (v0.22+)
-  filetypes = {
-    "css",
-    "graphql",
-    "html",
-    "javascript",
-    "javascriptreact",
-    "json",
-    "less",
-    "markdown",
-    "scss",
-    "typescript",
-    "typescriptreact",
-    "yaml",
-  },
+	bin = "prettier",
+	filetypes = {
+		"css",
+		"graphql",
+		"html",
+		"javascript",
+		"javascriptreact",
+		"json",
+		"less",
+		-- "markdown",
+		"scss",
+		"typescript",
+		"typescriptreact",
+		"yaml",
+	},
 })
