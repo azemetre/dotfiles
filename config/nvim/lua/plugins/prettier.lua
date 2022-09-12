a61e60bfc59dd1c346ad78951a3d1b530acfefdf
azemetre <azemetre@gmail.com>
2022-09-11 20:17:43 -0400

    nvim: removing prettier file type associations

--- config/nvim/lua/plugins/prettier.lua
+++ config/nvim/lua/plugins/prettier.lua
@@ -4,16 +4,12 @@ prettier.setup({
 	bin = "prettier",
 	filetypes = {
 		"css",
-		"graphql",
 		"html",
 		"javascript",
 		"javascriptreact",
 		"json",
-		"less",
-		-- "markdown",
 		"scss",
 		"typescript",
 		"typescriptreact",
-		"yaml",
 	},
 })
