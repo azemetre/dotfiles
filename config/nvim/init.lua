e1206214385f80d9c67f81559308112877c7976e
azemetre <azemetre@gmail.com>
2022-10-02 11:26:14 -0400

    nvim: harpoon hotkey tweaks

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -232,7 +232,7 @@ end
 -- plugin hotkeys
 -- harpoon
 nnoremap("<C-e>", ":lua require('harpoon.mark').add_file()<CR>")
-nnoremap("<C-r>", ":lua require('harpoon.ui').toggle_quick_menu()<CR>")
+nnoremap("<C-t>", ":lua require('harpoon.ui').toggle_quick_menu()<CR>")
 nnoremap("<C-h>", ":lua require('harpoon.ui').nav_file(1)<CR>")
 nnoremap("<C-j>", ":lua require('harpoon.ui').nav_file(2)<CR>")
 nnoremap("<C-k>", ":lua require('harpoon.ui').nav_file(3)<CR>")
