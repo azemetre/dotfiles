4a47c6cb4d5392e4285362c58f361a12a730010f
azemetre <azemetre@gmail.com>
2023-01-09 09:18:42 -0500

    nvim: begin again

--- config/nvim/lua/plugins/neogen.lua
+++
@@ -1,9 +0,0 @@
-local neogen = require("neogen")
-
-neogen.setup({
-	--if you want to disable Neogen
-	enabled = true,
-	-- (default: true) automatic jump (with insert mode) on inserted
-	-- annotation
-	input_after_comment = true,
-})
