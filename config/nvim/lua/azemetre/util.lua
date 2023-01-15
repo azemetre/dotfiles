a25f545eb616f029d3e78db472f06927976b1673
azemetre <azemetre@gmail.com>
2023-01-15 18:48:30 -0500

    nvim: unsure if func is necessary

--- config/nvim/lua/azemetre/util.lua
+++ config/nvim/lua/azemetre/util.lua
@@ -158,26 +158,26 @@ function M.get_root()
   return root
 end
 
--- this will return a function that calls telescope.
--- cwd will defautlt to lM.vim.util.get_root
--- for `files`, git_files or find_files will be chosen depending on .git
-function M.telescope(builtin, opts)
-  local params = { builtin = builtin, opts = opts }
-  return function()
-    builtin = params.builtin
-    opts = params.opts
-    opts = vim.tbl_deep_extend("force", { cwd = M.get_root() }, opts or {})
-    if builtin == "files" then
-      if vim.loop.fs_stat((opts.cwd or vim.loop.cwd()) .. "/.git") then
-        opts.show_untracked = true
-        builtin = "git_files"
-      else
-        builtin = "find_files"
-      end
-    end
-    require("telescope.builtin")[builtin](opts)
-  end
-end
+-- -- this will return a function that calls telescope.
+-- -- cwd will defautlt to lM.vim.util.get_root
+-- -- for `files`, git_files or find_files will be chosen depending on .git
+-- function M.telescope(builtin, opts)
+--   local params = { builtin = builtin, opts = opts }
+--   return function()
+--     builtin = params.builtin
+--     opts = params.opts
+--     opts = vim.tbl_deep_extend("force", { cwd = M.get_root() }, opts or {})
+--     if builtin == "files" then
+--       if vim.loop.fs_stat((opts.cwd or vim.loop.cwd()) .. "/.git") then
+--         opts.show_untracked = true
+--         builtin = "git_files"
+--       else
+--         builtin = "find_files"
+--       end
+--     end
+--     require("telescope.builtin")[builtin](opts)
+--   end
+-- end
 
 ---@param silent boolean?
 ---@param values? {[1]:any, [2]:any}
