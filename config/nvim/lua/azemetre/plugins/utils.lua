4fb3d46ee9f4850c26d7d320437ee6384f1bfd02
azemetre <azemetre@gmail.com>
2023-01-15 18:47:56 -0500

    nvim(hotkeys): more sane toggle

--- config/nvim/lua/azemetre/plugins/utils.lua
+++ config/nvim/lua/azemetre/plugins/utils.lua
@@ -1,47 +1,46 @@
 return {
 
-    -- measure startuptime
-    {
-        "dstein64/vim-startuptime",
-        cmd = "StartupTime",
-        config = function()
-            vim.g.startuptime_tries = 10
-        end,
-    },
+  -- measure startuptime
+  {
+    "dstein64/vim-startuptime",
+    cmd = "StartupTime",
+    config = function()
+      vim.g.startuptime_tries = 10
+    end,
+  },
 
-    -- session management
-    {
-        "folke/persistence.nvim",
-        event = "BufReadPre",
-        opts = { options = { "buffers", "curdir", "tabpages", "winsize", "help" } },
+  -- session management
+  {
+    "folke/persistence.nvim",
+    event = "BufReadPre",
+    opts = { options = { "buffers", "curdir", "tabpages", "winsize", "help" } },
         -- stylua: ignore
         keys = {
             { "<leader>qs", function() require("persistence").load() end, desc = "Restore Session" },
             { "<leader>ql", function() require("persistence").load({ last = true }) end, desc = "Restore Last Session" },
             { "<leader>qd", function() require("persistence").stop() end, desc = "Don't Save Current Session" },
         },
+  },
+
+  -- fugitive - git workflow
+  {
+    "tpope/vim-fugitive",
+    keys = {
+      { "<leader>Fq", ":q<CR>", desc = "Close Fugitive" },
+      { "<leader>FF", ":G<CR>", desc = "Git Status" },
+      { "<leader>Fb", ":G blame<CR>", desc = "Git Blame" },
+      { "<leader>Fp", ":G push<CR>", desc = "Git Push" },
     },
+  },
 
-    -- fugitive - git workflow
-    {
-        "tpope/vim-fugitive",
-        keys = {
-            { "<leader>Fq", ":q<CR>", desc = "Close Fugitive" },
-            { "<leader>Fs", ":G<CR>", desc = "Git Status" },
-            { "<leader>Fb", ":G blame<CR>", desc = "Git Blame" },
-            { "<leader>Fp", ":G push<CR>", desc = "Git Push" },
-        },
-    },
+  -- markdown preview
+  {
+    "iamcco/markdown-preview.nvim",
+    build = function()
+      vim.fn["mkdp#util#install"]()
+    end,
+  },
 
-    -- markdown preview
-    {
-        "iamcco/markdown-preview.nvim",
-        build = function()
-            vim.fn["mkdp#util#install"]()
-        end,
-
-    },
-
-    -- library used by other plugins
-    "nvim-lua/plenary.nvim",
+  -- library used by other plugins
+  "nvim-lua/plenary.nvim",
 }
