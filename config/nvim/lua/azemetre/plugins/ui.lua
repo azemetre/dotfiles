156783f2a35135058a87adbc0fbcf28a3b130b2a
azemetre <azemetre@gmail.com>
2023-01-16 11:30:55 -0500

    nvim(lazy): removing bufferline.nvim

--- config/nvim/lua/azemetre/plugins/ui.lua
+++ config/nvim/lua/azemetre/plugins/ui.lua
@@ -16,31 +16,6 @@ return {
     end,
   },
 
-  -- bufferline
-  {
-    "akinsho/nvim-bufferline.lua",
-    event = "BufAdd",
-    opts = {
-      options = {
-        diagnostics = "nvim_lsp",
-        always_show_bufferline = false,
-        diagnostics_indicator = function(_, _, diag)
-          local icons = require("azemetre.config.settings").icons.diagnostics
-          local ret = (diag.error and icons.Error .. diag.error .. " " or "")
-            .. (diag.warning and icons.Warn .. diag.warning or "")
-          return vim.trim(ret)
-        end,
-        offsets = {
-          {
-            filetype = "neo-tree",
-            text = "Neo-tree",
-            highlight = "Directory",
-            text_align = "left",
-          },
-        },
-      },
-    },
-  },
   -- feline -- statusline
   {
     "feline-nvim/feline.nvim",
@@ -337,7 +312,14 @@ return {
     opts = {
       -- char = "▏",
       char = "│",
-      filetype_exclude = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy" },
+      filetype_exclude = {
+        "help",
+        "alpha",
+        "dashboard",
+        "neo-tree",
+        "Trouble",
+        "lazy",
+      },
       show_trailing_blankline_indent = false,
       show_current_context = false,
     },
@@ -355,7 +337,15 @@ return {
     },
     config = function(_, opts)
       vim.api.nvim_create_autocmd("FileType", {
-        pattern = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy", "mason" },
+        pattern = {
+          "help",
+          "alpha",
+          "dashboard",
+          "neo-tree",
+          "Trouble",
+          "lazy",
+          "mason",
+        },
         callback = function()
           vim.b.miniindentscope_disable = true
         end,
@@ -380,12 +370,32 @@ return {
 
       dashboard.section.header.val = vim.split(logo, "\n")
       dashboard.section.buttons.val = {
-        dashboard.button("f", " " .. " Find file", ":Telescope find_files <CR>"),
-        dashboard.button("n", " " .. " New file", ":ene <BAR> startinsert <CR>"),
-        dashboard.button("r", " " .. " Recent files", ":Telescope oldfiles <CR>"),
-        dashboard.button("g", " " .. " Find text", ":Telescope live_grep <CR>"),
+        dashboard.button(
+          "f",
+          " " .. " Find file",
+          ":Telescope find_files <CR>"
+        ),
+        dashboard.button(
+          "n",
+          " " .. " New file",
+          ":ene <BAR> startinsert <CR>"
+        ),
+        dashboard.button(
+          "r",
+          " " .. " Recent files",
+          ":Telescope oldfiles <CR>"
+        ),
+        dashboard.button(
+          "g",
+          " " .. " Find text",
+          ":Telescope live_grep <CR>"
+        ),
         dashboard.button("c", " " .. " Config", ":e $MYVIMRC <CR>"),
-        dashboard.button("s", "勒" .. " Restore Session", [[:lua require("persistence").load() <cr>]]),
+        dashboard.button(
+          "s",
+          "勒" .. " Restore Session",
+          [[:lua require("persistence").load() <cr>]]
+        ),
         dashboard.button("l", "鈴" .. " Lazy", ":Lazy<CR>"),
         dashboard.button("q", " " .. " Quit", ":qa<CR>"),
       }
@@ -420,7 +430,11 @@ return {
         callback = function()
           local stats = require("lazy").stats()
           local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
-          dashboard.section.footer.val = "⚡ Neovim loaded " .. stats.count .. " plugins in " .. ms .. "ms"
+          dashboard.section.footer.val = "⚡ Neovim loaded "
+            .. stats.count
+            .. " plugins in "
+            .. ms
+            .. "ms"
           pcall(vim.cmd.AlphaRedraw)
         end,
       })
