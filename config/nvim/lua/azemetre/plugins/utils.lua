f82919baa2307f6a102b6e211f12492020a500e5
azemetre <azemetre@gmail.com>
2023-01-16 11:30:03 -0500

    chore: lua formatting

--- config/nvim/lua/azemetre/plugins/utils.lua
+++ config/nvim/lua/azemetre/plugins/utils.lua
@@ -15,11 +15,11 @@ return {
     event = "BufReadPre",
     opts = { options = { "buffers", "curdir", "tabpages", "winsize", "help" } },
         -- stylua: ignore
-        keys = {
-            { "<leader>qs", function() require("persistence").load() end, desc = "Restore Session" },
-            { "<leader>ql", function() require("persistence").load({ last = true }) end, desc = "Restore Last Session" },
-            { "<leader>qd", function() require("persistence").stop() end, desc = "Don't Save Current Session" },
-        },
+    keys = {
+        { "<leader>qs", function() require("persistence").load() end, desc = "Restore Session" },
+        { "<leader>ql", function() require("persistence").load({ last = true }) end, desc = "Restore Last Session" },
+        { "<leader>qd", function() require("persistence").stop() end, desc = "Don't Save Current Session" },
+    },
   },
 
   -- fugitive - git workflow
