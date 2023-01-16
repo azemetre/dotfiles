a79a9697540f02d716bd4dd2b0d5deca81949263
azemetre <azemetre@gmail.com>
2023-01-16 10:45:14 -0500

    chore: lua formatting

--- config/nvim/lua/azemetre/plugins/coding.lua
+++ config/nvim/lua/azemetre/plugins/coding.lua
@@ -1,18 +1,18 @@
 return {
 
-    -- snippets
-    {
-        "L3MON4D3/LuaSnip",
-        dependencies = {
-            "rafamadriz/friendly-snippets",
-            config = function()
-                require("luasnip.loaders.from_vscode").lazy_load()
-            end,
-        },
-        opts = {
-            history = true,
-            delete_check_events = "TextChanged",
-        },
+  -- snippets
+  {
+    "L3MON4D3/LuaSnip",
+    dependencies = {
+      "rafamadriz/friendly-snippets",
+      config = function()
+        require("luasnip.loaders.from_vscode").lazy_load()
+      end,
+    },
+    opts = {
+      history = true,
+      delete_check_events = "TextChanged",
+    },
         -- stylua: ignore
         keys = {
             {
@@ -25,145 +25,159 @@ return {
             { "<tab>", function() require("luasnip").jump(1) end, mode = "s" },
             { "<s-tab>", function() require("luasnip").jump(-1) end, mode = { "i", "s" } },
         },
-    },
+  },
 
-    -- auto completion
-    {
-        "hrsh7th/nvim-cmp",
-        event = "InsertEnter",
-        dependencies = {
-            "hrsh7th/cmp-nvim-lsp",
-            "hrsh7th/cmp-buffer",
-            "hrsh7th/cmp-path",
-            "saadparwaiz1/cmp_luasnip",
+  -- auto completion
+  {
+    "hrsh7th/nvim-cmp",
+    event = "InsertEnter",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "saadparwaiz1/cmp_luasnip",
+    },
+    opts = function()
+      local cmp = require("cmp")
+      return {
+        completion = {
+          completeopt = "menu,menuone,noinsert",
         },
-        opts = function()
-            local cmp = require("cmp")
-            return {
-                completion = {
-                    completeopt = "menu,menuone,noinsert",
-                },
-                snippet = {
-                    expand = function(args)
-                        require("luasnip").lsp_expand(args.body)
-                    end,
-                },
-                mapping = cmp.mapping.preset.insert({
-                    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-                    ["<C-f>"] = cmp.mapping.scroll_docs(4),
-                    ["<C-Space>"] = cmp.mapping.complete(),
-                    ["<C-e>"] = cmp.mapping.abort(),
-                    -- Accept currently selected item. Set `select` to `false`
-                    -- to only confirm explicitly selected items.
-                    ["<CR>"] = cmp.mapping.confirm({ select = true }),
-                }),
-                sources = cmp.config.sources({
-                    { name = "nvim_lsp" },
-                    { name = "luasnip" },
-                    { name = "buffer" },
-                    { name = "path" },
-                }),
-                formatting = {
-                    format = function(_, item)
-                        local icons = require("azemetre.config.settings").icons.kinds
-                        if icons[item.kind] then
-                            item.kind = icons[item.kind] .. item.kind
-                        end
-                        return item
-                    end,
-                },
-                experimental = {
-                    ghost_text = {
-                        hl_group = "LspCodeLens",
-                    },
-                },
-            }
-        end,
-    },
-
-    -- auto pairs
-    {
-        "echasnovski/mini.pairs",
-        event = "VeryLazy",
-        config = function(_, opts)
-            require("mini.pairs").setup(opts)
-        end,
-    },
-
-    -- surround
-    {
-        "echasnovski/mini.surround",
-        keys = { "gz" },
-        opts = {
-            mappings = {
-                add = "gza", -- Add surrounding in Normal and Visual modes
-                delete = "gzd", -- Delete surrounding
-                find = "gzf", -- Find surrounding (to the right)
-                find_left = "gzF", -- Find surrounding (to the left)
-                highlight = "gzh", -- Highlight surrounding
-                replace = "gzr", -- Replace surrounding
-                update_n_lines = "gzn", -- Update `n_lines`
-            },
+        snippet = {
+          expand = function(args)
+            require("luasnip").lsp_expand(args.body)
+          end,
         },
-        config = function(_, opts)
-            -- use gz mappings instead of s to prevent conflict with leap
-            require("mini.surround").setup(opts)
-        end,
-    },
-
-    -- comments
-    { "JoosepAlviste/nvim-ts-context-commentstring" },
-    {
-        "echasnovski/mini.comment",
-        event = "VeryLazy",
-        opts = {
-            hooks = {
-                pre = function()
-                    require("ts_context_commentstring.internal").update_commentstring({})
-                end,
-            },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-u>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-d>"] = cmp.mapping.scroll_docs(4),
+          ["<C-j>"] = cmp.mapping.select_next_item({
+            behavior = cmp.SelectBehavior.Insert,
+          }),
+          ["<C-k>"] = cmp.mapping.select_prev_item({
+            behavior = cmp.SelectBehavior.Insert,
+          }),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-e>"] = cmp.mapping.abort(),
+          -- Accept currently selected item. Set `select` to `false`
+          -- to only confirm explicitly selected items.
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+          { name = "buffer" },
+          { name = "path" },
+        }),
+        formatting = {
+          format = function(_, item)
+            local icons = require("azemetre.config.settings").icons.kinds
+            if icons[item.kind] then
+              item.kind = icons[item.kind] .. item.kind
+            end
+            return item
+          end,
         },
-        config = function(_, opts)
-            require("mini.comment").setup(opts)
-        end,
-    },
-
-    -- better text-objects
-    {
-        "echasnovski/mini.ai",
-        keys = {
-            { "a", mode = { "x", "o" } },
-            { "i", mode = { "x", "o" } },
+        experimental = {
+          ghost_text = {
+            hl_group = "LspCodeLens",
+          },
         },
-        dependencies = {
-            {
-                "nvim-treesitter/nvim-treesitter-textobjects",
-                init = function()
-                    -- no need to load the plugin, since we only need its queries
-                    require("lazy.core.loader").disable_rtp_plugin("nvim-treesitter-textobjects")
-                end,
-            },
-        },
-        opts = function()
-            local ai = require("mini.ai")
-            return {
-                n_lines = 500,
-                custom_textobjects = {
-                    o = ai.gen_spec.treesitter({
-                        a = { "@block.outer", "@conditional.outer", "@loop.outer" },
-                        i = { "@block.inner", "@conditional.inner", "@loop.inner" },
-                    }, {}),
-                    f = ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }, {}),
-                    c = ai.gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }, {}),
-                },
-            }
-        end,
-        config = function(_, opts)
-            local ai = require("mini.ai")
-            ai.setup(opts)
-        end,
-    },
+      }
+    end,
+  },
 
-    -- copilot
-    { "github/copilot.vim" },
+  -- auto pairs
+  {
+    "echasnovski/mini.pairs",
+    event = "VeryLazy",
+    config = function(_, opts)
+      require("mini.pairs").setup(opts)
+    end,
+  },
+
+  -- surround
+  {
+    "echasnovski/mini.surround",
+    keys = { "gz" },
+    opts = {
+      mappings = {
+        add = "gza", -- Add surrounding in Normal and Visual modes
+        delete = "gzd", -- Delete surrounding
+        find = "gzf", -- Find surrounding (to the right)
+        find_left = "gzF", -- Find surrounding (to the left)
+        highlight = "gzh", -- Highlight surrounding
+        replace = "gzr", -- Replace surrounding
+        update_n_lines = "gzn", -- Update `n_lines`
+      },
+    },
+    config = function(_, opts)
+      -- use gz mappings instead of s to prevent conflict with leap
+      require("mini.surround").setup(opts)
+    end,
+  },
+
+  -- comments
+  { "JoosepAlviste/nvim-ts-context-commentstring" },
+  {
+    "echasnovski/mini.comment",
+    event = "VeryLazy",
+    opts = {
+      hooks = {
+        pre = function()
+          require("ts_context_commentstring.internal").update_commentstring({})
+        end,
+      },
+    },
+    config = function(_, opts)
+      require("mini.comment").setup(opts)
+    end,
+  },
+
+  -- better text-objects
+  {
+    "echasnovski/mini.ai",
+    keys = {
+      { "a", mode = { "x", "o" } },
+      { "i", mode = { "x", "o" } },
+    },
+    dependencies = {
+      {
+        "nvim-treesitter/nvim-treesitter-textobjects",
+        init = function()
+          -- no need to load the plugin, since we only need its queries
+          require("lazy.core.loader").disable_rtp_plugin(
+            "nvim-treesitter-textobjects"
+          )
+        end,
+      },
+    },
+    opts = function()
+      local ai = require("mini.ai")
+      return {
+        n_lines = 500,
+        custom_textobjects = {
+          o = ai.gen_spec.treesitter({
+            a = { "@block.outer", "@conditional.outer", "@loop.outer" },
+            i = { "@block.inner", "@conditional.inner", "@loop.inner" },
+          }, {}),
+          f = ai.gen_spec.treesitter(
+            { a = "@function.outer", i = "@function.inner" },
+            {}
+          ),
+          c = ai.gen_spec.treesitter(
+            { a = "@class.outer", i = "@class.inner" },
+            {}
+          ),
+        },
+      }
+    end,
+    config = function(_, opts)
+      local ai = require("mini.ai")
+      ai.setup(opts)
+    end,
+  },
+
+  -- copilot
+  { "github/copilot.vim" },
 }
