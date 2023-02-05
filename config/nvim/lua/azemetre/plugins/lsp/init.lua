c1bc466c8b0b14d2b30a45d2a1c22f7f7ddea2ec
azemetre <azemetre@gmail.com>
2023-02-05 17:13:51 -0500

    nvim(lsp): staticcheck enabled for go

--- config/nvim/lua/azemetre/plugins/lsp/init.lua
+++ config/nvim/lua/azemetre/plugins/lsp/init.lua
@@ -30,6 +30,16 @@ return {
         stylelint_lsp = {
           filetypes = { "css", "less", "scss" },
         },
+				gopls = {
+					settings = {
+						gopls = {
+							analyses = {
+								unusedparams = true,
+							},
+							staticcheck = true,
+						},
+					},
+				}
       },
       -- you can do any additional lsp server setup here
       -- return true if you don't want this server to be setup with lspconfig
