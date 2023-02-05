745fe62468367c8c17542f497dde93681cbf687b
azemetre <azemetre@gmail.com>
2023-02-05 17:14:20 -0500

    nvim: adding removing go imports

--- config/nvim/lua/azemetre/config/autocmds.lua
+++ config/nvim/lua/azemetre/config/autocmds.lua
@@ -47,3 +47,23 @@ vim.api.nvim_create_autocmd("FileType", {
         vim.opt_local.spell = true
     end,
 })
+
+-- add/removes go imports
+vim.api.nvim_create_autocmd("BufWritePre", {
+	pattern = { "*.go" },
+	callback = function()
+		local ONE_SECOND = 1000
+		local params = vim.lsp.util.make_range_params()
+    params.context = { only = { "source.organizeImports" } }
+    local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, ONE_SECOND)
+    for _, res in pairs(result or {}) do
+			for _, r in pairs(res.result or {}) do
+				if r.edit then
+					vim.lsp.util.apply_workspace_edit(r.edit, "UTF-8")
+				else
+					vim.lsp.buf.execute_command(r.command)
+				end
+			end
+    end
+	end,
+})
