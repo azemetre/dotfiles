d9a4ccb38bf2d4d305995359dcb960ef83f12a01
azemetre <azemetre@gmail.com>
2023-05-26 10:11:59 -0400

    nvim(lsp): format is client disables it

--- config/nvim/lua/azemetre/plugins/lsp/format.lua
+++ config/nvim/lua/azemetre/plugins/lsp/format.lua
@@ -29,6 +29,15 @@ function M.format()
 end
 
 function M.on_attach(client, buf)
+	-- format if client disabled it
+	if
+		client.config
+		and client.config.capabilities
+		and client.config.capabilities.documentFormattingProvider == true
+	then
+		return
+	end
+
 	if client.supports_method("textDocument/formatting") then
 		vim.api.nvim_create_autocmd("BufWritePre", {
 			group = vim.api.nvim_create_augroup("LspFormat." .. buf, {}),
