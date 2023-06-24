e30524e95ddeba4997b59136dbe47edf363a67c8
azemetre <azemetre@gmail.com>
2023-06-24 16:26:25 -0400

    nvim(lsp): border on floating windows

--- config/nvim/lua/azemetre/plugins/lsp/init.lua
+++ config/nvim/lua/azemetre/plugins/lsp/init.lua
@@ -56,6 +56,22 @@ return {
 		},
 		---@param opts PluginLspOpts
 		config = function(plugin, opts)
+			local _border = "rounded"
+
+			vim.lsp.handlers["textDocument/hover"] =
+				vim.lsp.with(vim.lsp.handlers.hover, {
+					border = _border,
+				})
+
+			vim.lsp.handlers["textDocument/signatureHelp"] =
+				vim.lsp.with(vim.lsp.handlers.signature_help, {
+					border = _border,
+				})
+
+			vim.diagnostic.config({
+				float = { border = _border },
+			})
+
 			if plugin.servers then
 				require("azemetre.util").deprecate(
 					"lspconfig.servers",
