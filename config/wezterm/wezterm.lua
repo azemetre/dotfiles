eb7aa101bb38201df5a0f13e4dfe4dc2c3ec762b
azemetre <azemetre@gmail.com>
2023-09-22 08:35:52 -0400

    term: initial wezterm config

---
+++ config/wezterm/wezterm.lua
@@ -0,0 +1,30 @@
+local wezterm = require("wezterm")
+
+local config = {
+	-- window_background_opacity = 0.15,
+	macos_window_background_blur = 30,
+	enable_tab_bar = false,
+	window_decorations = "RESIZE",
+	font = wezterm.font("Hack Nerd Font Mono"),
+	font_size = 24,
+	adjust_window_size_when_changing_font_size = true,
+	native_macos_fullscreen_mode = true,
+	keys = {
+		{
+			key = "n",
+			mods = "SHIFT|CTRL",
+			action = wezterm.action.ToggleFullScreen,
+		},
+	},
+	window_padding = {
+		left = 0,
+		right = 0,
+		top = 0,
+		bottom = 0,
+	},
+	send_composed_key_when_left_alt_is_pressed = true,
+	send_composed_key_when_right_alt_is_pressed = false,
+	color_scheme = "Tokyo Night Storm",
+}
+
+return config
