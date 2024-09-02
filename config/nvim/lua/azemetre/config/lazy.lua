fcfe3628f304661326ec8ade754e878b619d02e8
azemetre <azemetre@gmail.com>
2024-09-02 10:24:28 -0400

    nvim(config): toggle changes, nvim 0.10 detecttion, new init

--- config/nvim/lua/azemetre/config/lazy.lua
+++ config/nvim/lua/azemetre/config/lazy.lua
@@ -1,48 +1,42 @@
+-- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({
 		"git",
 		"clone",
 		"--filter=blob:none",
-		"--single-branch",
-		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazyrepo,
 		lazypath,
 	})
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
 end
-vim.opt.runtimepath:prepend(lazypath)
+vim.opt.rtp:prepend(lazypath)
 
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+vim.g.mapleader = ","
+vim.g.maplocalleader = ","
+
+-- Setup lazy.nvim
 require("lazy").setup({
-	spec = { import = config_namespace .. ".plugins" },
-	dev = {
-		path = "~/.config/nvim",
-		patterns = { "azemetre" },
+	spec = {
+		-- import your plugins
+		{ import = "azemetre.plugins" },
 	},
-	checker = {
-		enabled = true,
-		notify = false,
-		frequency = 900,
-	},
-	change_detection = {
-		enabled = true,
-		notify = false,
-	},
-	ui = {
-		icons = {
-			plugin = "",
-		},
-	},
-	performance = {
-		rtp = {
-			disabled_plugins = {
-				"gzip",
-				-- "matchit", -- TODO: definitely use this I think, may need to tweak
-				"matchparen",
-				-- "netrwPlugin", -- TODO: definitely use this I think, may need to tweak
-				"tarPlugin",
-				"tohtml",
-				"tutor",
-				"zipPlugin",
-			},
-		},
-	},
+	-- Configure any other settings here. See the documentation for more details.
+	-- colorscheme that will be used when installing plugins.
+	install = { colorscheme = { "habamax" } },
+	-- automatically check for plugin updates
+	checker = { enabled = true },
 })
