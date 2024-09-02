e07469cab6fc17211d3409b47266f9f50c111038
azemetre <azemetre@gmail.com>
2024-09-02 10:27:53 -0400

    nvim: adding dap, lang specifics settings

---
+++ config/nvim/lua/azemetre/plugins/extras/lang/toml.lua
@@ -0,0 +1,14 @@
+return {
+  recommended = function()
+	 return Azemetre.extras.wants({
+      ft = "toml",
+      root = "*.toml",
+    })
+  end,
+  "neovim/nvim-lspconfig",
+  opts = {
+    servers = {
+      taplo = {},
+    },
+  },
+}
