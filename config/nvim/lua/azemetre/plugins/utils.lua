9cfef494738fa785ecc9b1e56cb6d5d3d32ca38f
azemetre <azemetre@gmail.com>
2024-09-03 11:42:21 -0400

    nvim: vim-startup-time no longer necessary with lazy.nvim

--- config/nvim/lua/azemetre/plugins/utils.lua
+++ config/nvim/lua/azemetre/plugins/utils.lua
@@ -1,14 +1,4 @@
 return {
-
-	-- measure startuptime
-	{
-		"dstein64/vim-startuptime",
-		cmd = "StartupTime",
-		config = function()
-			vim.g.startuptime_tries = 10
-		end,
-	},
-
 	-- session management
 	{
 		"folke/persistence.nvim",
