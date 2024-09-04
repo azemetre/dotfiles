db9a2e642458e45f92c488c54d602933b37cfc74
azemetre <azemetre@gmail.com>
2024-09-04 10:22:53 -0400

    nvim(lazy): properly load all relevant plugins

--- config/nvim/lua/azemetre/config/lazy.lua
+++ config/nvim/lua/azemetre/config/lazy.lua
@@ -33,10 +33,11 @@ require("lazy").setup({
 	spec = {
 		-- import your plugins
 		{ import = "azemetre.plugins" },
+		{ import = "azemetre.plugins.extras.dap.core" },
+		{ import = "azemetre.plugins.extras.test.core" },
+		{ import = "azemetre.plugins.extras.lang" },
 	},
 	-- Configure any other settings here. See the documentation for more details.
-	-- colorscheme that will be used when installing plugins.
-	install = { colorscheme = { "habamax" } },
 	-- automatically check for plugin updates
-	checker = { enabled = true },
+	checker = { enabled = false },
 })
