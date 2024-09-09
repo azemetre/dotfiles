0660a67278352daf839c5854c2511cf2db20613d
azemetre <azemetre@gmail.com>
2024-09-09 11:56:59 -0400

    chore: lua formatting

--- config/nvim/lua/azemetre/plugins/extras/dap/core.lua
+++ config/nvim/lua/azemetre/plugins/extras/dap/core.lua
@@ -65,15 +65,12 @@ return {
 
 			for name, sign in pairs(Azemetre.config.icons.dap) do
 				sign = type(sign) == "table" and sign or { sign }
-				vim.fn.sign_define(
-					"Dap" .. name,
-					{
-						text = sign[1],
-						texthl = sign[2] or "DiagnosticInfo",
-						linehl = sign[3],
-						numhl = sign[3],
-					}
-				)
+				vim.fn.sign_define("Dap" .. name, {
+					text = sign[1],
+					texthl = sign[2] or "DiagnosticInfo",
+					linehl = sign[3],
+					numhl = sign[3],
+				})
 			end
 
 			-- setup dap config by VsCode launch.json file
