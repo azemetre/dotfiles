55042a206525950c3095c5be2d753a7c64c7f21f
azemetre <azemetre@gmail.com>
2024-09-09 10:36:54 -0400

    nvim(coding): comments should be native behavior
    Testing workflow to ensure that these libraries are no longer necessary.

--- config/nvim/lua/azemetre/plugins/coding.lua
+++ config/nvim/lua/azemetre/plugins/coding.lua
@@ -147,29 +147,29 @@ return {
 	},
 
 	-- comments
-	{
-		"JoosepAlviste/nvim-ts-context-commentstring",
-		event = "VeryLazy",
-		opts = {
-			enable_autocmd = false,
-		},
-		config = function(_, opts)
-			require("ts_context_commentstring").setup(opts)
-		end,
-	},
-	{
-		"echasnovski/mini.comment",
-		event = "VeryLazy",
-		opts = {
-			custom_commentstring = function()
-				return require("ts_context_commentstring").calculate_commentstring()
-					or vim.bo.commentstring
-			end,
-		},
-		config = function(_, opts)
-			require("mini.comment").setup(opts)
-		end,
-	},
+	-- {
+	-- 	"JoosepAlviste/nvim-ts-context-commentstring",
+	-- 	event = "VeryLazy",
+	-- 	opts = {
+	-- 		enable_autocmd = false,
+	-- 	},
+	-- 	config = function(_, opts)
+	-- 		require("ts_context_commentstring").setup(opts)
+	-- 	end,
+	-- },
+	-- {
+	-- 	"echasnovski/mini.comment",
+	-- 	event = "VeryLazy",
+	-- 	opts = {
+	-- 		custom_commentstring = function()
+	-- 			return require("ts_context_commentstring").calculate_commentstring()
+	-- 				or vim.bo.commentstring
+	-- 		end,
+	-- 	},
+	-- 	config = function(_, opts)
+	-- 		require("mini.comment").setup(opts)
+	-- 	end,
+	-- },
 
 	-- better text-objects
 	{
