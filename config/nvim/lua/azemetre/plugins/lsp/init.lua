04a526aa853412c90b6182ab3fa01e0eca167c2a
azemetre <azemetre@gmail.com>
2024-10-09 15:51:30 -0400

    nvim(cmp): adding cmdline

--- config/nvim/lua/azemetre/plugins/lsp/init.lua
+++ config/nvim/lua/azemetre/plugins/lsp/init.lua
@@ -37,8 +37,8 @@ return {
 				-- Be aware that you also will need to properly configure your LSP server to
 				-- provide the inlay hints.
 				inlay_hints = {
-					enabled = true,
-					exclude = { "vue" }, -- filetypes for which you don't want to enable inlay hints
+					enabled = false,
+					exclude = { "rust" }, -- filetypes for which you want to enable inlay hints
 				},
 				-- Enable this to enable the builtin LSP code lenses on Neovim >= 0.10.0
 				-- Be aware that you also will need to properly configure your LSP server to
