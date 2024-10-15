d89a053ab85f547895a52197e62df38a8da32c76
azemetre <azemetre@gmail.com>
2024-10-15 09:28:27 -0400

    nvim(ufo): hiding signs, in-line hint

--- config/nvim/lua/azemetre/plugins/editor.lua
+++ config/nvim/lua/azemetre/plugins/editor.lua
@@ -371,21 +371,44 @@ return {
 			provider_selector = function(bufnr, filetype, buftype)
 				return { "lsp", "indent" }
 			end,
-			-- You can add more UFO-specific options here
-			-- For example:
-			-- open_fold_hl_timeout = 150,
-			-- close_fold_kinds = { "imports", "comment" },
-			-- preview = {
-			--     win_config = {
-			--         border = { "", "─", "", "", "", "─", "", "" },
-			--         winhighlight = "Normal:Folded",
-			--         winblend = 0,
-			--     },
-			-- },
+			fold_virt_text_handler = function(
+				virtText,
+				lnum,
+				endLnum,
+				width,
+				truncate
+			)
+				local newVirtText = {}
+				local suffix = ("\t⤵ %d lines folded ⤵"):format(endLnum - lnum)
+				local sufWidth = vim.fn.strdisplaywidth(suffix)
+				local targetWidth = width - sufWidth
+				local curWidth = 0
+				for _, chunk in ipairs(virtText) do
+					local chunkText = chunk[1]
+					local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+					if targetWidth > curWidth + chunkWidth then
+						table.insert(newVirtText, chunk)
+					else
+						chunkText = truncate(chunkText, targetWidth - curWidth)
+						local hlGroup = chunk[2]
+						table.insert(newVirtText, { chunkText, hlGroup })
+						chunkWidth = vim.fn.strdisplaywidth(chunkText)
+						-- str width returned from truncate() may less than 2nd argument, need padding
+						if curWidth + chunkWidth < targetWidth then
+							suffix = suffix
+								.. (" "):rep(targetWidth - curWidth - chunkWidth)
+						end
+						break
+					end
+					curWidth = curWidth + chunkWidth
+				end
+				table.insert(newVirtText, { suffix, "MoreMsg" })
+				return newVirtText
+			end,
 		},
 		config = function(_, opts)
 			-- Fold options
-			vim.o.foldcolumn = "1"
+			vim.o.foldcolumn = "0"
 			vim.o.foldlevel = 99 -- Using ufo provider need a large value
 			vim.o.foldlevelstart = 99
 			vim.o.foldenable = true
