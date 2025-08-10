29dac76fffbc93e3092aee928224ee20e324f9fa
azemetre <azemetre@gmail.com>
2025-08-10 02:42:50 -0400

    nvim(oil): using same icons as `nvim_tree.lua`

--- config/nvim/lua/azemetre/plugins/oil.lua
+++ config/nvim/lua/azemetre/plugins/oil.lua
@@ -28,6 +28,6 @@ return {
 			},
 		},
 		-- Optional dependencies
-		dependencies = { { "echasnovski/mini.icons", opts = {} } },
+		dependencies = { { "nvim-tree/nvim-web-devicons", opts = {} } },
 	},
 }
