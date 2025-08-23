e4e6a4f75960687c4d2224f589964962e402b14b
azemetre <azemetre@gmail.com>
2025-08-23 12:57:18 -0400

    nvim(lsp): setting up marksman
    feat: creating root marksman file

---
+++ config/nvim/lsp/marksman.lua
@@ -0,0 +1,25 @@
+return {
+	default_config = {
+		cmd = { "marksman", "server" },
+		filetypes = { "markdown", "markdown.mdx", ".md" },
+		root_markers = {
+			".marksman.toml",
+			".git", -- git
+			".hg", -- mercurial
+			".svn", -- subversion
+			".bzr", -- bazaar
+		},
+		single_file_support = true,
+	},
+	docs = {
+		description = [[
+https://github.com/artempyanykh/marksman
+
+Marksman is a Markdown LSP server providing completion, cross-references, diagnostics, and more.
+
+Marksman works on MacOS, Linux, and Windows and is distributed as a self-contained binary for each OS.
+
+Pre-built binaries can be downloaded from https://github.com/artempyanykh/marksman/releases
+]],
+	},
+}
