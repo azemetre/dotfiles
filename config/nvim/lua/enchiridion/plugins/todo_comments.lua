80db2000fa1145fa60b6480ffb533834083b6d42
azemetre <azemetre@gmail.com>
2025-08-30 11:11:45 -0400

    nvim(todo): removing toggle trouble keymap

--- config/nvim/lua/enchiridion/plugins/todo_comments.lua
+++ config/nvim/lua/enchiridion/plugins/todo_comments.lua
@@ -4,6 +4,7 @@
 -- TODO:
 -- FIX:
 -- NOTE:
+-- INFO:
 -- WARNING:
 return {
 	"folke/todo-comments.nvim",
@@ -15,14 +16,36 @@ return {
 		},
 	},
 	config = true,
-		-- stylua: ignore
-		keys = {
-			{ "]t", function() require("todo-comments").jump_next() end, desc = "Next todo comment" },
-			{ "[t", function() require("todo-comments").jump_prev() end, desc = "Previous todo comment" },
-			{ "<leader>xt", "<cmd>TodoTrouble<cr>", desc = "Todo Trouble" },
-			{ "<leader>xtt", "<cmd>TodoTrouble keywords=TODO,FIX,FIXME<cr>", desc = "Todo Trouble" },
-			{ "<leader>xf", "<cmd>TroubleToggle<cr>", desc = "Toggle Trouble" },
-			{ "<leader>xd", "<cmd>FzfLua diagnostics_document<cr>", desc = "Document Diagnostics" },
-			{ "<leader>xw", "<cmd>FzfLua diagnostics_workspace<cr>", desc = "Workspace Diagnostics" },
+	keys = {
+		{
+			"]t",
+			function()
+				require("todo-comments").jump_next()
+			end,
+			desc = "Next todo comment",
 		},
+		{
+			"[t",
+			function()
+				require("todo-comments").jump_prev()
+			end,
+			desc = "Previous todo comment",
+		},
+		{ "<leader>xt", "<cmd>TodoTrouble<cr>", desc = "Todo Trouble" },
+		{
+			"<leader>xtt",
+			"<cmd>TodoTrouble keywords=TODO,FIX,FIXME<cr>",
+			desc = "Todo Trouble",
+		},
+		{
+			"<leader>fd",
+			"<cmd>FzfLua diagnostics_document<cr>",
+			desc = "Document Diagnostics",
+		},
+		{
+			"<leader>fw",
+			"<cmd>FzfLua diagnostics_workspace<cr>",
+			desc = "Workspace Diagnostics",
+		},
+	},
 }
