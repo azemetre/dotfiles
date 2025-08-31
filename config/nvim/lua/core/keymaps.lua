298fa3ede02b12041062f32fcd81445c98bec022
azemetre <azemetre@gmail.com>
2025-08-31 14:51:51 -0400

    docs: explaining some concepts

--- config/nvim/lua/core/keymaps.lua
+++ config/nvim/lua/core/keymaps.lua
@@ -1,20 +1,22 @@
--- FIXME: document all keymaps
-
 ---@type vim.keymap.set.Opts
 local options = { silent = true }
 
--- Clear search with <esc> or <cr>
+-- NOTE: think of the `mapleader` as your staff, every vimmer has one tailored
+-- to their needs. Don't be afraid to try out different staffs, use what makes
+-- you comfortable when casting motions.
+vim.g.mapleader = ","
+
 vim.keymap.set(
 	{ "i", "n" },
 	"<esc>",
 	"<cmd>noh<cr><esc>",
-	{ desc = "Escape and clear hlsearch" }
+	{ desc = "escape and clear hlsearch" }
 )
 vim.keymap.set(
 	{ "n" },
 	"<leader><cr>",
 	"<cmd>noh<cr>",
-	{ desc = "Enter and clear hlsearch" }
+	{ desc = "enter and clear hlsearch" }
 )
 vim.keymap.set("n", "gw", "*N")
 vim.keymap.set("x", "gw", "*N")
@@ -24,99 +26,100 @@ vim.keymap.set(
 	"n",
 	"n",
 	"'Nn'[v:searchforward]",
-	{ expr = true, desc = "Next search result" }
+	{ expr = true, desc = "next Search result" }
 )
 vim.keymap.set(
 	"x",
 	"n",
 	"'Nn'[v:searchforward]",
-	{ expr = true, desc = "Next search result" }
+	{ expr = true, desc = "next Search result" }
 )
 vim.keymap.set(
 	"o",
 	"n",
 	"'Nn'[v:searchforward]",
-	{ expr = true, desc = "Next search result" }
+	{ expr = true, desc = "next Search result" }
 )
 vim.keymap.set(
 	"n",
 	"N",
 	"'nN'[v:searchforward]",
-	{ expr = true, desc = "Prev search result" }
+	{ expr = true, desc = "prev Search result" }
 )
 vim.keymap.set(
 	"x",
 	"N",
 	"'nN'[v:searchforward]",
-	{ expr = true, desc = "Prev search result" }
+	{ expr = true, desc = "prev Search result" }
 )
 vim.keymap.set(
 	"o",
 	"N",
 	"'nN'[v:searchforward]",
-	{ expr = true, desc = "Prev search result" }
+	{ expr = true, desc = "prev Search result" }
 )
 
--- visually move selection
 vim.keymap.set(
 	{ "v", "o" },
 	"J",
 	":m '>+1<CR>gv=gv",
-	{ desc = "Move visual selection down" }
+	{ desc = "move Visual Selection down" }
 )
 vim.keymap.set(
 	{ "v", "o" },
 	"K",
 	":m '<-2<CR>gv=gv",
-	{ desc = "Move visual selection up" }
+	{ desc = "move Visual Selection up" }
 )
 
 vim.keymap.set(
 	"n",
 	"<leader>xl",
 	"<cmd>lopen<cr>",
-	{ desc = "Open Location List" }
+	{ desc = "open Location List" }
 )
 vim.keymap.set(
 	"n",
 	"<leader>xf",
 	"<cmd>copen<cr>",
-	{ desc = "Open Quickfix List" }
+	{ desc = "open Quickfix List" }
 )
 
--- quit
-vim.keymap.set("n", "<leader>qq", "<cmd>qa<cr>", { desc = "Quit all" })
+vim.keymap.set(
+	"n",
+	"<leader>qq",
+	"<cmd>qa<cr>",
+	{ desc = "Quit all, does not save" }
+)
 
--- highlights under cursor
 vim.keymap.set(
 	"n",
 	"<leader>hl",
 	vim.show_pos,
-	{ desc = "Highlight Groups at cursor" }
+	{ desc = "Highlight Groups under cursor" }
 )
 
--- buffers
 vim.keymap.set(
 	"n",
 	"<leader>b]",
 	"<cmd>:BufferLineCycleNext<CR>",
-	{ desc = "Next Buffer" }
+	{ desc = "next Buffer" }
 )
 vim.keymap.set(
 	"n",
 	"<leader>bb",
 	"<cmd>:e #<cr>",
-	{ desc = "Switch to Other Buffer" }
-)
-vim.keymap.set(
-	"n",
-	"<leader>b[",
-	"<cmd>:BufferLineCyclePrev<CR>",
-	{ desc = "Previous Buffer" }
+	{ desc = "alternate previous Buffer" }
 )
 vim.keymap.set(
 	"n",
 	"<leader>`",
 	"<cmd>:e #<cr>",
-	{ desc = "Switch to Other Buffer" }
+	{ desc = "alternate previous Buffer" }
+)
+vim.keymap.set(
+	"n",
+	"<leader>b[",
+	"<cmd>:BufferLineCyclePrev<CR>",
+	{ desc = "previous Buffer" }
 )
